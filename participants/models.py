a44b4e01f6ba2640b838ea264e3e03d00e5b4a6a
Rauhan Aziz <rauhan_aziz@yahoo.co.uk>
2026-09-07 13:13:33 +0100

    Add participant model and model choices

--- participants/models.py
+++ participants/models.py
@@ -1,3 +1,50 @@
 from django.db import models
+from studies.models import Study
 
-# Create your models here.
+class Participant(models.Model):
+
+    class Sex(models.TextChoices):
+          MALE = "Male", "Male"
+          FEMALE = "Female", "Female"
+          
+
+    class Status(models.TextChoices):
+          SCREENING = "Screening", "Screening"
+          ENROLLED = "Enrolled", "Enrolled"
+          ACTIVE = "Active", "Active"
+          COMPLETED = "Completed", "Completed"
+          WITHDRAWN = "Withdrawn", "Withdrawn"
+          SCREEN_FAILED = "Screen Failed", "Screen Failed"
+
+
+
+    study = models.ForeignKey(
+      Study,
+      on_delete=models.CASCADE,
+      related_name='participants'
+    )
+    participant_number = models.CharField(
+      max_length=50,
+      unique=True,
+    )
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    date_of_birth = models.DateField()
+    sex = models.CharField(
+          max_length=10,
+          choices=Sex.choices,
+      )
+
+    status = models.CharField(
+          max_length=50,
+          choices=Status.choices,
+      )
+    enrolled_date = models.DateField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+          return (
+              f"{self.participant_number} - "
+              f"{self.first_name} {self.last_name}"
+          )
--- studies/models.py
+++ studies/models.py
@@ -3,11 +3,33 @@ from django.db import models
 from cloudinary_storage.storage import RawMediaCloudinaryStorage
 
 class Study(models.Model):
+
+    class Phase(models.TextChoices):
+        PHASE_1 = "Phase 1", "Phase 1"
+        PHASE_2 = "Phase 2", "Phase 2"
+        PHASE_3 = "Phase 3", "Phase 3"
+        PHASE_4 = "Phase 4", "Phase 4"
+
+    class Status(models.TextChoices):
+        PLANNING = "Planning", "Planning"
+        RECRUITING = "Recruiting", "Recruiting"
+        ACTIVE = "Active", "Active"
+        COMPLETED = "Completed", "Completed"
+        SUSPENDED = "Suspended", "Suspended"
+        TERMINATED = "Terminated", "Terminated"
+        
     protocol_number = models.CharField(max_length=100, unique=True)
     title = models.CharField(max_length=255)
     description = models.TextField(blank=True)
-    phase = models.CharField(max_length=50, blank=True)
-    status = models.CharField(max_length=50)
+    phase = models.CharField(
+        max_length=50,
+        choices=Phase.choices,
+    )
+
+    status = models.CharField(
+        max_length=50,
+        choices=Status.choices,
+    )
     start_date = models.DateField(null=True, blank=True)
     end_date = models.DateField(null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
@@ -18,6 +40,19 @@ class Study(models.Model):
 
 
 class StudyDocument(models.Model):
+
+    class Category(models.TextChoices):
+        PROTOCOL = "Protocol", "Protocol"
+        INVESTIGATOR_BROCHURE = (
+            "Investigator Brochure",
+            "Investigator Brochure",
+        )
+        INFORMED_CONSENT = "Informed Consent Form", "Informed Consent Form"
+        SITE_DOCUMENT = "Site Document", "Site Document"
+        TRAINING_DOCUMENT = "Training Document", "Training Document"
+        OTHER = "Other", "Other"
+
+    
     study = models.ForeignKey(
         Study,
         on_delete=models.CASCADE,
@@ -33,7 +68,10 @@ class StudyDocument(models.Model):
         upload_to='study_documents/',
         storage=RawMediaCloudinaryStorage(),
     )
-    category = models.CharField(max_length=100)
+    category = models.CharField(
+        max_length=100,
+        choices=Category.choices,
+    )
     version = models.CharField(max_length=50)
     uploaded_at = models.DateTimeField(auto_now_add=True)
 
