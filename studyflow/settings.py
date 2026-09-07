0347ae521e469774f76de74a7a958fa3675ee891
Rauhan Aziz <rauhan_aziz@yahoo.co.uk>
2026-09-07 11:43:55 +0100

    Create core studyflow django apps

--- studyflow/settings.py
+++ studyflow/settings.py
@@ -42,6 +42,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'accounts',
+    'studies',
+    'participants',
+    'notifications'
 ]
 
 MIDDLEWARE = [
