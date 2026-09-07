4362cc3ca03ae63f13dc714c5f5e925114ca386a
Rauhan Aziz <rauhan_aziz@yahoo.co.uk>
2026-09-07 12:51:20 +0100

    Add study document model

--- studies/models.py
+++ studies/models.py
@@ -1,5 +1,6 @@
+from django.contrib.auth.models import User
 from django.db import models
-
+from cloudinary_storage.storage import RawMediaCloudinaryStorage
 
 class Study(models.Model):
     protocol_number = models.CharField(max_length=100, unique=True)
@@ -14,3 +15,27 @@ class Study(models.Model):
 
     def __str__(self):
         return f"{self.protocol_number} - {self.title}"
+
+
+class StudyDocument(models.Model):
+    study = models.ForeignKey(
+        Study,
+        on_delete=models.CASCADE,
+        related_name='documents'
+    )
+    uploaded_by = models.ForeignKey(
+        User,
+        on_delete=models.SET_NULL,
+        null=True,
+        related_name='uploaded_study_documents'
+    )
+    file = models.FileField(
+        upload_to='study_documents/',
+        storage=RawMediaCloudinaryStorage(),
+    )
+    category = models.CharField(max_length=100)
+    version = models.CharField(max_length=50)
+    uploaded_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.study.protocol_number} - {self.file.name}'
