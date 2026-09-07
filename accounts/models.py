049098bfec6c56e2f9b6c92711d25220b417fa9e
Rauhan Aziz <rauhan_aziz@yahoo.co.uk>
2026-09-07 12:17:39 +0100

    Add user study assignment model

--- accounts/models.py
+++ accounts/models.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import User
 from django.db import models
+from studies.models import Study
 
 
 class Role(models.Model):
@@ -30,3 +31,36 @@ class UserProfile(models.Model):
 
     def __str__(self):
         return self.user.username
+
+
+class UserStudy(models.Model):
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name="study_assignments",
+    )
+    study = models.ForeignKey(
+        Study,
+        on_delete=models.CASCADE,
+        related_name="user_assignments",
+    )
+    assigned_by = models.ForeignKey(
+        User,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name="assigned_studies",
+    )
+    assigned_at = models.DateTimeField(auto_now_add=True)
+    is_active = models.BooleanField(default=True)
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=["user", "study"],
+                name="unique_user_study_assignment",
+            )
+        ]
+
+    def __str__(self):
+        return f"{self.user.username} - {self.study.protocol_number}"
