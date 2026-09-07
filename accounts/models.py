b18463c16e0c802b1c5c3705c0688ebbb8837c15
Rauhan Aziz <rauhan_aziz@yahoo.co.uk>
2026-09-07 11:56:47 +0100

    Add role and user profile models

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,32 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Role(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    description = models.CharField(max_length=255, blank=True)
+    is_admin = models.BooleanField(default=False)
+    can_approve_users = models.BooleanField(default=False)
+    can_manage_studies = models.BooleanField(default=False)
+    can_manage_users = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.name
+
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(
+        User,
+        on_delete=models.CASCADE,
+        related_name="profile",
+    )
+    role = models.ForeignKey(
+        Role,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+    )
+    is_approved = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.user.username
