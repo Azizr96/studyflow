ec2c71a9aecc5eb57cbf95510b023e969e611559
Rauhan Aziz <rauhan_aziz@yahoo.co.uk>
2026-09-07 13:59:17 +0100

    Add notification model

--- notifications/models.py
+++ notifications/models.py
@@ -1,3 +1,32 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+
+class Notification(models.Model):
+
+    class Type(models.TextChoices):
+        VISIT_REMINDER = "Visit Reminder", "Visit Reminder"
+        PARTICIPANT_ADDED = "Participant Added", "Participant Added"
+        DOCUMENT_UPLOADED = "Document Uploaded", "Document Uploaded"
+        VISIT_COMPLETED = "Visit Completed", "Visit Completed"
+        STUDY_ASSIGNED = "Study Assigned", "Study Assigned"
+        GENERAL = "General", "General"
+
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name="notifications",
+    )
+    title = models.CharField(max_length=150)
+    message = models.TextField()
+    type = models.CharField(
+        max_length=50,
+        choices=Type.choices,
+        default=Type.GENERAL,
+    )
+    is_read = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user.username} - {self.title}"
