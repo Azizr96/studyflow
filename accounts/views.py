c1c78bd348d92fd2b59ebe90a2d45ac5e8dc3b04
Rauhan Aziz <rauhan_aziz@yahoo.co.uk>
2026-09-07 14:35:57 +0100

    Add user registration

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.shortcuts import redirect, render
 
-# Create your views here.
+from .forms import RegistrationForm
+
+
+def register(request):
+    if request.method == "POST":
+        form = RegistrationForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+
+            messages.success(
+                request,
+                (
+                    "Your account has been created successfully. "
+                    "Please wait for administrator approval."
+                ),
+            )
+
+            return redirect("accounts:register")
+
+    else:
+        form = RegistrationForm()
+
+    context = {
+        "form": form,
+    }
+
+    return render(
+        request,
+        "accounts/register.html",
+        context,
+    )
