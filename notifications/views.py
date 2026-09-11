bd2efde686850a82fa3e8fa55f1d8e7e2a9777ba
Rauhan Aziz <rauhan_aziz@yahoo.co.uk>
2026-09-11 11:04:15 +0100

    Add notification list view functionw ith render

--- notifications/views.py
+++ notifications/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from django.shortcuts import get_object_or_404, redirect, render
+from .models import Notification
 
-# Create your views here.
+
+@login_required
+def notification_list(request):
+    notifications = Notification.objects.filter(
+        user=request.user,
+    ).order_by("-created_at")
+
+    unread_count = notifications.filter(
+        is_read=False,
+    ).count()
+
+    context = {
+        "notifications": notifications,
+        "unread_count": unread_count,
+    }
+
+    return render(
+        request,
+        "notifications/notification_list.html",
+        context,
+    )
+
+
